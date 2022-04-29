f586c5950acd23f8ccc431390a9ef4e87925d072
Brian <a43310175@gmail.com>
2022-04-29 15:46:41 +0800

    examing 2(Delivery)

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,19 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int price;
+    int shipFeed;
+
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed = delivery.price;
     }
+
+    public int total(){
+        return (amount+delivery.price);
+    }
+
+
 }
